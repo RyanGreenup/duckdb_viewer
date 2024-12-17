591c62dfaf6874274a6a874ed5a165d619ab0f62
ryan <exogenesis@protonmail.com>
2024-12-17 11:43:26 +1100

    fix: Update index method signature in TableListModel

--- duckdb_browser/main.py
+++ duckdb_browser/main.py
@@ -56,7 +56,7 @@ class TableListModel(QAbstractItemModel):
         return None
 
     def index(
-        self, row: int, column: int, parent: QModelIndex = QModelIndex()
+        self, row: int, column: int, parent: Union[QModelIndex, QPersistentModelIndex] = QModelIndex()
     ) -> QModelIndex:
         if self.hasIndex(row, column, parent):
             return self.createIndex(row, column)
